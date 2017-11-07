baff1339cecb66a3db11138a8cfa3584ecea7e4f
王龙 <wanglong@ishangzu.com>
2017-11-07 21:16:29 +0800

    去掉无用注解

--- src/main/java/com/platform/WorkApplication.java
+++ src/main/java/com/platform/WorkApplication.java
@@ -3,13 +3,11 @@ package com.platform;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import us.codecraft.webmagic.Spider;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class WorkApplication implements CommandLineRunner {
 	/** 
